542473488c33f06bab29551b28cde71a953d57bc
Ace Atienza <ace@ace.com>
2013-02-18 19:02:39 -0500

    add sentences and word_split methods

--- string_extend.rb
+++ string_extend.rb
@@ -1,5 +1,18 @@
 class String
+	
+	# Scan for vowels, ignoring case
 	def vowels
 		scan(/[aeiou]/i)
 	end
+
+	# Split text into sentences based on period \. followed by any whitespace \s*
+	def sentences
+		gsub(/\n|\r/, ' ').split(/\.\s*/)
+	end
+
+	# Split sentences into words, even words with \' or \-
+	def word_split
+		scan(/\w[\w\'\-]*/)
+	end
+
 end
